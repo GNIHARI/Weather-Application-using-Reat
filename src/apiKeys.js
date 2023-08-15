1f638ef50661f7d8b2ec408b696e341974b37bc7
Nihari <niharig333@gmail.com>
2023-08-15 13:44:53 +0530

    Initial commit

---
+++ src/apiKeys.js
@@ -0,0 +1,6 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+    key: "a915dfbd956282975d62d43aa55d6184",
+    base: "https://api.openweathermap.org/data/2.5/",
+  };
+  
